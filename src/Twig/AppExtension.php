d1f806fe60de3ac5cffc02aadf7435ccc92cfb0e
Roberto Bonilla <robertostevenb@gmail.com>
2022-08-17 23:44:26 -0500

    Filter fizzBuzz

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,7 +2,18 @@
 
 namespace App\Twig;
 
-class AppExtension
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class AppExtension extends AbstractExtension
 {
+    public function getFilters()
+    {
+        return [
+            // the logic of this filter is now implemented in a different class
+            new TwigFilter('fizzBuzz', [FizzBuzzRuntime::class, 'fizzBuzzFilter']),
+        ];
+    }
 
 }
--- src/Twig/FizzBuzzRuntime.php
+++ src/Twig/FizzBuzzRuntime.php
@@ -2,7 +2,47 @@
 
 namespace App\Twig;
 
-class FizzBuzzRuntime
-{
+use App\Entity\Fizzbuzz;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Twig\Extension\RuntimeExtensionInterface;
 
+class FizzBuzzRuntime implements RuntimeExtensionInterface
+{
+    /**
+     * @param int $startNumber
+     * @param int $endNumber
+     *
+     * @return string
+     */
+    public function fizzBuzzFilter($startNumber, $endNumber = 0)
+    {
+        if ($endNumber == 0) {
+            $endNumber = $startNumber + 30;
+        }
+
+        $fizzBuzzSeries = "";
+
+        for ($i = $startNumber; $i <= $endNumber; $i++) {
+            $isFizz = (0 === $i % 3);
+            $isBuzz = (0 === $i % 5);
+
+            if (!$isFizz && !$isBuzz) {
+                $fizzBuzzSeries .= $i.", ";
+                continue;
+            }
+
+            if ($isFizz) {
+                $fizzBuzzSeries .= "Fizz";
+            }
+
+            if ($isBuzz) {
+                $fizzBuzzSeries .= "Buzz";
+            }
+
+            $fizzBuzzSeries .= ", ";
+        }
+
+        return $fizzBuzzSeries;
+    }
 }
